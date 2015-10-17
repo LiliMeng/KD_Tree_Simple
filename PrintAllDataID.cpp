ffc9cca74feaff377e77df1fa3361222ae74b6b9
LiliMeng <lilimeng1103@gmail.com>
2015-10-17 13:30:49 -0700

    Update PrintAllDataID.cpp

--- PrintAllDataID.cpp
+++ PrintAllDataID.cpp
@@ -1,5 +1,5 @@
 /**
- * File: PrintDataID.cpp
+ * File: PrintAllDataID.cpp
  * Author: Lili Meng (lilimeng1103@gmail.com)
  * Print out the ID of the point where the ID is the row in the original file(0-based)
  * The format is very simple:
